878c7b408844da073e9ec1753d4fa8404348ae75
Roberto <robertobelt@Yahoo.es>
2021-07-05 15:56:59 -0400

    Moved get_user_info into userInfo class

--- code/SteamChameleon.py
+++ code/SteamChameleon.py
@@ -30,33 +30,6 @@ def FindImg(soup):
 def GetImg(imageLink):
     urllib.request.urlretrieve(imageLink, "ProfilePictureCopy")
 
-
-
-def get_user_info():
-    steam_login = input("Enter your Steam ID: ")
-    steam_pwd = input("Enter your Steam Password: ")
-    user = userInfo(steam_login,steam_pwd)
-    user_consent = input("Would you like to setup automatic email steam verification?\n"
-      "Enter Y or N:\n")
-    incorrect_input = True
-    while incorrect_input:
-        if user_consent.upper() == "Y":
-            email_login = input("Please enter your email address: ")
-            email_password = input("Please enter your email password: ")
-            user.set_email_info(email_login, email_password)
-            user.email_consent = True
-            incorrect_input = False
-        
-        elif user_consent.upper() == "N":
-            user.email_consent = False
-            incorrect_input = False
-
-        else:
-            user_consent = input("Incorrect Input: Please enter Y or N:\n")
-            incorrect_input = True
-    return user
-
-
 def get_steam_guard(email_login, email_password):
         
         IMAP_server = "imap.mail.yahoo.com"
--- code/user_manager.py
+++ code/user_manager.py
@@ -22,4 +22,27 @@ class UserManager:
         4. Email Login: %s
         5. Email Password: %s""" % (user.steam_login, user.steam_password, user.email_consent,
         user.email_login, user.email_password)
-        
+    
+    def get_user_info():
+        steam_login = input("Enter your Steam ID: ")
+        steam_pwd = input("Enter your Steam Password: ")
+        user = userInfo(steam_login,steam_pwd)
+        user_consent = input("Would you like to setup automatic email steam verification?\n"
+        "Enter Y or N:\n")
+        incorrect_input = True
+        while incorrect_input:
+            if user_consent.upper() == "Y":
+                email_login = input("Please enter your email address: ")
+                email_password = input("Please enter your email password: ")
+                user.set_email_info(email_login, email_password)
+                user.email_consent = True
+                incorrect_input = False
+            
+            elif user_consent.upper() == "N":
+                user.email_consent = False
+                incorrect_input = False
+
+            else:
+                user_consent = input("Incorrect Input: Please enter Y or N:\n")
+                incorrect_input = True
+        return user
